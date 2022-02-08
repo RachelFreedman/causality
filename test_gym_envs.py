18aeee68775b4620a139e7c3327a40f8f7120ccc
Jeremy Tien <jeremy29tien@gmail.com>
2022-02-08 12:02:27 -0800

    blah

---
+++ test_gym_envs.py
@@ -0,0 +1,8 @@
+import gym
+env = gym.make('Reacher-v2')
+env.reset()
+for _ in range(5000):
+    env.render()
+    observation, reward, done, info = env.step(env.action_space.sample()) # take a random action
+    print("done", done)
+env.close()
