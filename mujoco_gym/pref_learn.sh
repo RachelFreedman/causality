8bc3f54c293309fd23d9011d78e991c0c195e06b
Jeremy Tien <jeremy29tien@gmail.com>
2022-06-02 12:39:06 -0400

    make changes for better tuned wd and more data vanilla run

--- mujoco_gym/pref_learn.sh
+++ mujoco_gym/pref_learn.sh
@@ -8,18 +8,18 @@ for seed in 0 1 2; do
 
   #Reward-learning
   echo "Reward learning..."
-  config="feature_sensitivity/${var1}raw_augmentedfull_linear_2000prefs_60pairdelta_100epochs_10patience_001lr_01l1reg"
+  config="vanilla/324demos_hdim128-64_stateaction_2000prefs_100epochs_10patience_001lr_00001weightdecay"
   reward_model_path="/home/jeremy/gym/trex/models/${config}_seed${seed}.params"
   reward_output_path="reward_learning_outputs/${config}_seed${seed}.txt"
 
   cd trex/
-  python3 model.py --augmented_full --num_rawfeatures ${var1} --num_comps 2000 --pair_delta 60 --num_epochs 100 --patience 10 --lr 0.01 --l1_reg 0.1 --seed $seed --reward_model_path $reward_model_path > $reward_output_path
+  python3 model.py --hidden_dims 128 64 --state_action --num_demos 324 --num_comps 2000 --num_epochs 100 --patience 10 --lr 0.01 --weight_decay 0.0001 --seed $seed --reward_model_path $reward_model_path > $reward_output_path
 
   #RL
   echo "Performing RL..."
   cd ..
   policy_save_dir="./trained_models_reward_learning/${config}_seed${seed}"
-  python3 mujoco_gym/learn.py --env "ReacherLearnedReward-v0" --algo sac --seed $seed --train --train-timesteps 1000000 --reward-net-path $reward_model_path --indvar ${var1} --save-dir $policy_save_dir
+  python3 mujoco_gym/learn.py --env "ReacherLearnedReward-v0" --algo sac --seed $seed --train --train-timesteps 1000000 --reward-net-path $reward_model_path --save-dir $policy_save_dir
 
   #Eval
   echo "Evaluating RL..."
