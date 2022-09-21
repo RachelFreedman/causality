217cc014bb8b02bfad2a4f4cd62bb2674f93ab88
Jeremy Tien <jeremy29tien@gmail.com>
2022-09-20 23:09:33 -0700

    add stuff for reacher and debug

--- calc_kls.py
+++ calc_kls.py
@@ -8,10 +8,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('--indvar_str', default='', help="")
     parser.add_argument('--indvar_num', default=0.0, type=float, help="")
-    parser.add_argument('--config', default='', help="")  # ex.: "feeding/vanilla/324demos_allpairs_hdim256-256-256_100epochs_10patience_00001lr_0000001weightdecay"
+    parser.add_argument('--config', default='', help="")
     args = parser.parse_args()
 
     config = args.config
+    fully_observable = False
+    pure_fully_observable = False
+    if "pure_fully_observable" in config:
+        pure_fully_observable = True
+    else:
+        fully_observable = True
 
     results = dict()
     results['train_accs'] = []
@@ -20,26 +26,29 @@ if __name__ == '__main__':
     results['dkl_qp'] = []
     results['symmetric_dkl'] = []
     for seed in [0, 1, 2]:
-        env = "FeedingSawyer-v1"
-        prefix = "/home/jeremy/assistive-gym/"
+        env = "Reacher-v2"
+        prefix = "/home/jeremy/gym/"
 
-        reward_learning_data_path = prefix + "trex/data/feeding/fully_observable/demos.npy"
+        if fully_observable:
+            reward_learning_data_path = prefix + "trex/data/reacher/raw_stateaction/raw_360/demos.npy"
+        else:
+            reward_learning_data_path = prefix + "trex/data/reacher/pure_fully_observable/demos.npy"
 
-        trained_policy_path = prefix + "trained_models_reward_learning/" + config + "_seed" + str(seed) + "/ppo/FeedingLearnedRewardSawyer-v0/checkpoint_40/checkpoint-40"
+        trained_policy_path = prefix + "trained_models_reward_learning/" + config + "_seed" + str(seed) + "/sac/ReacherLearnedReward-v0/checkpoint_002231/checkpoint-2231"
         discriminator_model_path = prefix + "discriminator_kl_models/" + config + "_seed" + str(seed) + ".params"
 
         train_acc, val_acc, dkl_pq, dkl_qp = discriminator_kl.run(env, seed, reward_learning_data_path, trained_policy_path,
-                                                                  num_trajs=50, fully_observable=True, pure_fully_observable=False,
+                                                                  num_trajs=50, fully_observable=fully_observable, pure_fully_observable=pure_fully_observable,
                                                                   load_weights=True, discriminator_model_path=discriminator_model_path,
                                                                   num_epochs=100, hidden_dims=(128, 128, 128), lr=0.01,
                                                                   weight_decay=0.0001, l1_reg=0.0, patience=10)
-        results['train_accs'].append(train_acc)
-        results['val_accs'].append(val_acc)
-        results['dkl_pq'].append(dkl_pq)
-        results['dkl_qp'].append(dkl_qp)
-        results['symmetric_dkl'].append(dkl_pq+dkl_qp)
+        results['train_accs'].append(float(train_acc))
+        results['val_accs'].append(float(val_acc))
+        results['dkl_pq'].append(float(dkl_pq))
+        results['dkl_qp'].append(float(dkl_qp))
+        results['symmetric_dkl'].append(float(dkl_pq + dkl_qp))
 
-    results['avg_symmetric_dkl'] = np.mean(results['symmetric_dkl'])
+    results['avg_symmetric_dkl'] = float(np.mean(results['symmetric_dkl']))
 
     outfile = prefix+"discriminator_kl_outputs/"+config+"_results.json"
     with open(outfile, 'w') as f:
